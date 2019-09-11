56e84b47e4ace8666b7c037c293d4870791df6cf
Wenbin Hou <catchyrime@outlook.com>
2019-09-11 22:24:32 +0800

    Add create_submit.sh

---
+++ create_submit.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -v
+set -e
+
+cd "$(dirname "$0")"
+
+rm -f submit.zip
+rm -rf submit
+mkdir submit
+cd submit
+
+
+# Copy source code
+cp -a ../src sourcecode
+rm -f sourcecode/*.o sourcecode/*.out
+
+# Copy README
+cp -a ../README.md readme.txt
+
+# Generate run.sh
+echo '#!/bin/bash -e' >run.sh
+echo 'cd "$(dirname "$0")"' >>run.sh
+echo 'exec ./BDCI19.out "$@"' >>run.sh
+chmod +x run.sh
+
+# Generate compile.sh
+echo '#!/bin/bash -e' >compile.sh
+echo 'cd "$(dirname "$0")"' >>compile.sh
+echo 'make -j -C sourcecode' >>compile.sh
+chmod +x compile.sh
+
+# Compile and copy binary file
+./compile.sh
+mv sourcecode/BDCI19.out .
+
+
+# Create zip file
+tar -cf ../submit.zip *
+
+cd ..
+rm -rf submit
