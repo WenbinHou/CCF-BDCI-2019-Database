ec3073fd95c096c2f0583d53168fcd8fdb40ec58
Wenbin Hou <catchyrime@outlook.com>
2019-10-08 21:14:51 +0800

    Update create_submit.sh

--- create_submit.sh
+++ create_submit.sh
@@ -5,43 +5,18 @@ set -e
 
 cd "$(dirname "$0")"
 
-rm -f submit.zip
-rm -rf submit
+rm -rf submit.tar.gz submit
 mkdir submit
 cd submit
 
-
 # Copy source code
-cp -a ../src ./src
-cp -a ../CMakeLists.txt ./
-rm -f src/*.o src/*.out
+cp -a ../sourcecode ../run.sh ../compile.sh ../README.md ./
 
-# Copy README
-cp -a ../README.md readme.txt
-
-# Generate run.sh
-echo '#!/bin/bash -e' >run.sh
-echo 'cd "$(dirname "$0")"' >>run.sh
-echo 'exec ./BDCI19 "$@"' >>run.sh
-chmod +x run.sh
-
-# Generate compile.sh
-echo '#!/bin/bash -ve' >compile.sh
-echo 'cd "$(dirname "$0")"' >>compile.sh
-echo 'rm -rf build; mkdir build' >>compile.sh
-echo 'cd build' >>compile.sh
-echo 'cmake .. -DCMAKE_BUILD_TYPE=Release -DMAKE_FASTEST=ON' >>compile.sh
-echo 'make -j' >>compile.sh
-echo 'mv BDCI19 ../' >>compile.sh
-echo 'cd ..; rm -rf build' >>compile.sh
-chmod +x compile.sh
-
-# Compile and copy binary file
+# Compile and generate binary
 ./compile.sh
 
-
-# Create zip file
-tar -cf ../submit.tar.gz *
-
+# Create compressed file
 cd ..
+tar -cf submit.tar.gz submit
+
 rm -rf submit
